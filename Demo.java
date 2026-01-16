f1adf95252e611c4a51f0b0405007217eb73e7e8
SaiRishik07 <sairishikkoli7@gmail.com>
2026-01-16 16:00:07 +0530

    Dynamic Array

--- Demo.java
+++ Demo.java
@@ -1,20 +1,16 @@
-class Calculater {
-    int a ;
-    public int add(int n1,int n2){
-       int r = n1 + n2;
-       return r ; 
-    }
-}
 
 public class Demo {
-    
+
     public static void main(String[] args) {
-        int num1=5;
-        int num2=6;
-        
-        Calculater clac = new Calculater();
-         int result = clac.add(num1, num2);
-        System.out.println(result);
+        int num[] = new int[4];
+        num[0] = 3;
+        num[1] = 4;
+        num[2] = 5;
+        num[3] = 8;
+    for(int i=0;i<4;i++){
+        System.out.println(num[i]);
+    }
 
     }
+
 }
