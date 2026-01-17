d6c4126a54b6edfdfd1fa6ccf3bd9cf7d8c1e535
SaiRishik07 <sairishikkoli7@gmail.com>
2026-01-17 18:11:51 +0530

    Array of Object

---
+++ object.java
@@ -0,0 +1,31 @@
+class Student {
+    String Name;
+    int  roll;
+   
+}
+
+
+
+public class object {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+          s1.Name = "Sai";
+          s1.roll = 19;
+        Student s2 = new Student();
+          s2.Name = "ishan";
+          s2.roll = 10; 
+         Student s3 = new Student();
+          s3.Name = "Aditya";
+          s3.roll = 13;
+        
+        Student students[] = new Student[3];
+        students[0]= s1;
+        students[1]= s2;
+        students[2]= s3;
+        for(int i=0;i<students.length;i++){
+            System.out.println(students[i].Name +":"+students[i].roll);
+        }
+            
+        
+    }
+}
