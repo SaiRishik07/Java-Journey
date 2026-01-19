06f4c1c11d72812bbb4801d31b94f917828e6089
SaiRishik07 <sairishikkoli7@gmail.com>
2026-01-19 16:12:05 +0530

    Inheritance

---
+++ AdvCalc.java
@@ -0,0 +1,8 @@
+public class AdvCalc extends Calc {
+    public int Multiply(int n1,int n2){
+        return n1*n2;
+    }
+    public int Divide(int n1,int n2){
+        return n1/n2;
+    }
+}
---
+++ Inheritance.java
@@ -0,0 +1,11 @@
+public class Inheritance {
+    public static void main(String[] args) {
+        AdvCalc obj1 = new AdvCalc();
+        int r1 = obj1.add(5, 6);
+        int r2 = obj1.sub(5, 6);
+        int r3 = obj1.Multiply(5, 6);
+        int r4 = obj1.Divide(5, 6);
+
+     System.out.println(r1 +":"+ r2+":"+r3+":"+r4);
+    }
+}
